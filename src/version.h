a843c64b5cb70452b99a2f700d0dad213df451a6
JJ Uzumaki <104718343+xpunk7@users.noreply.github.com>
2022-05-28 16:01:20 -0400

    Create version.h

---
+++ src/version.h
@@ -0,0 +1,6 @@
+#pragma once
+
+extern const char* const EXOHOOD_VERSION_TAG;
+extern const char* const EXOHOOD_VERSION;
+extern const char* const EXOHOOD_RELEASE_NAME;
+extern const char* const EXOHOOD_VERSION_FULL;
